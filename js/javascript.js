96f6d1e17ddfdea3bcb79028627c6a5edf9dbc9f
Gabriel Kaique <gabrielkaique_contato@hotmail.com>
2022-05-02 19:49:20 -0300

    Removendo comentários do código js

--- js/javascript.js
+++ js/javascript.js
@@ -8,13 +8,6 @@
     var t4 = '';
     var t5 = '';
 
-    /*As "chaves" de criptografia que utilizaremos são:
-        `A letra "e" é convertida para "enter"`
-        `A letra "i" é convertida para "imes"`
-        `A letra "a" é convertida para "ai"`
-        `A letra "o" é convertida para "ober"`
-        `A letra "u" é convertida para "ufat"`*/
-
     function substituirCaracteres(){
 
         t1 = input.value.replaceAll('e','enter');
